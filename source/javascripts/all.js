b67a8a277b88166f9a4eab38071d210f9def3e19
Arthur Gunn <arthur@gunn.co.nz>
2013-08-28 19:39:38 +1200

    Update references.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,3 @@
 //= require '../../vendor/javascripts/jquery-1.10.2'
 //= require '../../vendor/javascripts/bootstrap/tooltip'
-//= require_tree .
+//= require_tree '../../vendor/javascripts/bootstrap'
